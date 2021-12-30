87309076821f1b30b6ef8d3815c68edff638c8d3
MatheusTGamerPro ツ <89700288+MatheusTGamerPro@users.noreply.github.com>
2021-12-30 01:15:51 -0300

    Atualização do Script instalador Termux
    # Adições
    - Adicionar Paleta de Cores para o Terminal.
    - Trocar URL do instalador (Fonte)
    
    # Correções
    - Corrigir Erro: '1-/ unknow'  pelo URL
    - Trocar Fonte de instalação antiga

--- Termux-Installer/Termux-Installer.py
+++ Termux-Installer/Termux-Installer.py
@@ -8,15 +8,24 @@ except ModuleNotFoundError:
 	Shell('pip install wget')
 
 # Termux Installer For Android
+# Colors for Terminal (Theme)
+
+R = '\033[01;31m'
+G = '\033[01;32m'
+Y = '\033[01;33m'
+B = '\033[01;34m'
+M = '\033[01;35m'
+C = '\033[01;36m'
+W = '\033[01;37m'
 
 def Termux_Installer ():
 	Choice = input("\033[01;35mDeseja Baixar o Apk do Termux [y/n]: \033[01;33m")
 	if Choice == "y":
-		print("\033[01;38mPesquisando Site de Download...")
+		print(W,"Pesquisando Site de Download...")
 		
 		sleep(2)
 
-		URL_Get = 'https://cdn.down-apk.com/com.termux/Termux_0.117_apkcombo.com.apk?ecp=Y29tLnRlcm11eC8wLjExNy8xMTcuNDkxZjIwN2UyODlhYzA1YmNiMzljYTQzNmI1MjE4ZjZhZTgwMWRiZC5hcGs=&iat=1640053074&sig=a5441081fd3e6ccf05dfa039e852f177&size=85749239&from=cf&version=latest'
+		URL_Get = 'https://cdn.down-apk.com/com.termux/Termux_0.117_apkcombo.com.apk?ecp=Y29tLnRlcm11eC8wLjExNy8xMTcuNDkxZjIwN2UyODlhYzA1YmNiMzljYTQzNmI1MjE4ZjZhZTgwMWRiZC5hcGs=&iat=1640837450&sig=207f6223508fa8e1ac1cfb4957c9ded1&size=85749239&from=cf&version=latest'
 		
 		try:
 			CreateDiretory('/sdcard/Termux-Apk')
@@ -27,12 +36,12 @@ def Termux_Installer ():
 		
 		sleep(1)
 	
-		print("\n \033[01;32mIniciando Download do APK...")
+		print(G,"\nIniciando Download do APK...",W)
 	
 		Get(URL_Get, Local)
 	
 		print(f"\nAPK do Termux Baixado com sucesso, vá até: {Local}")
 	else:
-		print("\033[01;31mInstalaçao Cancelada.")
+		print(R,"Instalaçao Cancelada.",W)
 
 Termux_Installer()
